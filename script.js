5ecf3f7bf8f801314dbd4b40aef634cfe0e6eff2
Eduardo Wiater <kpooh7766@gmail.com>
2021-06-15 11:18:30 +0200

    Feature: add console joke response

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const url = "https://icanhazdadjoke.com/";
+const boton = document.getElementById("boton");
 
+boton.addEventListener("click", function getJokes() {
+  fetch(url, {
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data.joke));
+});
