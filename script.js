e1d164ca5fac09da8ca1cfa033dacd287ea4bb38
Eduardo Wiater <kpooh7766@gmail.com>
2021-06-20 00:35:07 +0200

    Feature: Add weather response to html

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-const url = "https://icanhazdadjoke.com/";
+const urlJoke = "https://icanhazdadjoke.com/";
+const urlWeather = 'https://www.el-tiempo.net/api/json/v2/provincias/08/municipios/08019';
+
 const boton = document.getElementById("boton");
 
+function clima (){
+  fetch(urlWeather , {
+    headers: {
+      'Accept': 'application/json'
+     }
+  })
+    .then( response => response.json())
+    .then( data => document.getElementById("tiempo").innerHTML = data.stateSky.description)
+}
+
 boton.addEventListener("click", function getJokes() {
-  fetch(url, {
+  fetch(urlJoke, {
     headers: {
       Accept: "application/json",
     },
@@ -10,3 +22,5 @@ boton.addEventListener("click", function getJokes() {
     .then((response) => response.json())
     .then((data) =>  document.getElementById('results').innerHTML = data.joke );
 });
+
+clima();  // Preguntar : Asi esta bien o deberia utilizar un eventlistener(load)??
